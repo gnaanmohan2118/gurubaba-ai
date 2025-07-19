87a14699ce4655f8bd7e86ec769241e0aeb82b1b
gnaanmohan2118 <87522635+gnaanmohan2118@users.noreply.github.com>
2025-07-19 21:54:15 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -4,4 +4,4 @@ from dotenv import load_dotenv
 load_dotenv()
 
 GROQ_API_KEY= os.getenv("GROQ_API_KEY")
-GROQ_API_BASE = os.getenv("GROQ_API_BASE", "https://api.perplexity.ai/chat/completions")
+GROQ_API_BASE = os.getenv("GROQ_API_BASE", "https://api.groq.com/openai/v1/chat/completions")
